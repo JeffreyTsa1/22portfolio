31d6b8f43787462003875ca8e2f6bf8d2035fafa
Jeffrey Tsai <jeffreytsai@Jeffreys-MacBook-Pro.local>
2022-11-02 14:34:56 -0700

    First Commit

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="aboutWrapper">
+      <div id="about">
+        <h1>Hi, I'm Jeffrey.</h1>
+        <p>
+            Welcome to my website! Below, you will find a few projects that I have worked on in the past. I am a recent graduate from the University of Illinois, currently seeking full-time opportunities as a Software Engineer. I specialize in building elegant products for the web. 
+        </p>
+        <p>
+            I'm Taiwanese-Canadian, but I lived in Shanghai and Hong Kong for the majority of my life. Outside of work, I'm a professional DJ, and I tinker with cars.
+        </p>
+        <p> Please feel free to <a href="mailto::jeffrey.my.tsai@gmail.com">reach out to me</a> if you have any questions or if you would like to collaborate!</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
